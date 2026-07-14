60f24419600b39caff1ac8a6f3a57bc8577b92d3
alexd-ev <alexdev719@gmail.com>
2026-07-14 16:16:29 +1000

    Update `Island.cpp` to fix tree growing constantly

--- src/Island.cpp
+++ src/Island.cpp
@@ -79,9 +79,9 @@ namespace skyblock_generator {
     void Island::placeTree(mcpp::Coordinate treeCoord) {
         setTreeHelperBlocks(treeCoord, mcpp::Blocks::STONE);
         setIslandBlock(treeCoord, mcpp::Blocks::OAK_SAPLING);
-        setRandomTickSpeed("10000");
+        setRandomTickSpeed("20000");
         setItemDespawnTime("1");
-        std::this_thread::sleep_for(std::chrono::seconds(5));
+        std::this_thread::sleep_for(std::chrono::seconds(10));
         setRandomTickSpeed("3");
         setItemDespawnTime("6000");
         setTreeHelperBlocks(treeCoord, mcpp::Blocks::AIR);
@@ -91,10 +91,12 @@ namespace skyblock_generator {
     }
 
     void Island::setTreeHelperBlocks(mcpp::Coordinate treeCoord, mcpp::BlockType block) const {
+        setIslandBlock({ treeCoord.x - 1, treeCoord.y + 1, treeCoord.z }, block);
         setIslandBlock({ treeCoord.x - 1, treeCoord.y + 2, treeCoord.z }, block);
-        setIslandBlock({ treeCoord.x, treeCoord.y + 2, treeCoord.z - 1 }, block);
-        setIslandBlock({ treeCoord.x + 1, treeCoord.y + 2, treeCoord.z }, block);
-        setIslandBlock({ treeCoord.x, treeCoord.y + 2, treeCoord.z + 1 }, block);
+        setIslandBlock({ treeCoord.x - 1, treeCoord.y + 3, treeCoord.z }, block);
+        setIslandBlock({ treeCoord.x, treeCoord.y + 3, treeCoord.z - 1 }, block);
+        setIslandBlock({ treeCoord.x + 1, treeCoord.y + 3, treeCoord.z }, block);
+        setIslandBlock({ treeCoord.x, treeCoord.y + 3, treeCoord.z + 1 }, block);
         setIslandBlock({ treeCoord.x, treeCoord.y + 8, treeCoord.z }, block);
     }
 
